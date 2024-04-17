3916e7bca00c0bfe68b648a65c1f6dfb4a130e98
austindecker <deckaj01@pfw.edu>
2024-04-17 14:35:08 -0400

    started on heap implementation.

--- heapstructure/heap.c
+++ heapstructure/heap.c
@@ -1,2 +1,60 @@
 #include "heap.h"
 
+Node* node_create(void* data, int order){
+    Node* newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->order = order;
+
+    return newNode;
+}
+
+Heap* heap_create(int heap_size, int (*order_function)(void* item), void (*del_function)(void* item)){
+    Heap* newHeap = malloc(sizeof(Heap));
+    newHeap->max_size = heap_size;
+    newHeap->order_func = order_function;
+    newHeap->del_func = del_function;
+    newHeap->nextIndex = 0;
+
+    Node** heapStorage = malloc(heap_size * sizeof(Node*));
+
+    if(heapStorage == NULL){
+        return NULL;
+    }
+
+    newHeap->heapStorage = heapStorage;
+    return newHeap;
+}
+
+Heap* heap_build(void* array, int array_size, int heap_size, int (*order_function)(void* item), void (*del_function)(void* item)){
+
+}
+
+void heap_destroy(Heap* heap){
+    for (int i = 0; i < heap->nextIndex; i++)
+    {
+       Node* currentNode = heap->heapStorage[i];
+       heap->del_func(currentNode->data);
+       free(currentNode);
+    }
+    free(heap->heapStorage);
+    
+}
+
+void heap_add(Heap* self, void* item){
+    if(!(self->nextIndex < self->max_size)){
+        return;
+    }
+
+    int order = self->order_func(item);
+    Node* newNode = node_create(item, order);
+
+    heapify_down(self);
+}
+
+void* heap_remove(Heap* self){
+    if(self->nextIndex == 0){
+        return NULL;
+    }
+
+    
+}
--- heapstructure/heap.h
+++ heapstructure/heap.h
@@ -15,15 +15,44 @@ struct Heap {
     Node** heapStorage;
     int max_size;
     int nextIndex;
+    int (*order_func)(void* item);
+    void (*del_func)(void* item);
+
     
 };
 typedef struct Heap Heap;
 
+/*
+creates a locally used node object that wraps the void pointer data. Also contains an integer value representing the order of the node.
+*/
 Node* node_create(void* data, int order);
-Heap* heap_create(int heap_size);
-Heap* heap_create(void* array, int array_size, int heap_size);
+/*
+initializes an empty heap array representation with functions to manipulate the internal array.
+must include custom del and order functions in order to properly handle whatever data the heap will hold.
+*/
+Heap* heap_create(int heap_size, int (*order_function)(void* item), void (*del_function)(void* item));
+/*
+destroys the heap and frees any allocated memory
+*/
+void heap_destroy(Heap* self);
+/*
+initializes a heap given an array of items. Items are heapified.
+*/
+Heap* heap_build(void* array, int array_size, int heap_size, int (*order_function)(void* item), void (*del_function)(void* item));
+/*
+add an item to the heap if space is available.
+*/
 void heap_add(Heap* self, void* data);
+/*
+remove the root item from the heap if there is an item to remove.
+*/
 void* heap_remove(Heap* self);
+/*
+returns an array of void pointers in ascending order based on the order.
+*/
 void* heap_sort(Heap* self);
-
+/*
+returns the maximum size of the heap.
+*/
+int heap_size(Heap* self);
 #endif
